07d5fd02306dcb26e636ce9532df0cc35c13f1dd
chintan <chintan.tops@gmail.com>
2023-04-30 23:06:50 +0530

    oprators

---
+++ Learning_corejava/src/oprators/O001_Arithmatic.java
@@ -0,0 +1,23 @@
+package oprators;
+
+public class O001_Arithmatic {
+	public static void main(String[] args) {
+		
+		
+		double a = 23;
+		double b = 10;
+		
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
+		
+		
+		System.out.println("Test"+a);
+		System.out.println(a+"test");
+		System.out.println("Test"+a+b);
+		System.out.println(a+b+"Test"+a+b);
+		
+	}
+}
