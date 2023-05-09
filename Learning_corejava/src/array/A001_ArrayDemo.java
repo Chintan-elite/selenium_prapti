2d49ebba9c9a6525b1033d51951cb4c082e7ad8c
chintan <chintan.tops@gmail.com>
2023-05-09 07:18:28 +0530

    array

---
+++ Learning_corejava/src/array/A001_ArrayDemo.java
@@ -0,0 +1,62 @@
+package array;
+
+public class A001_ArrayDemo {
+	public static void main(String[] args) {
+		
+		
+//		int a[] = new int[5]; //delaration
+//		
+//		//inialisation
+//		a[0] = 20;
+//		a[1] = 30;
+//		a[2] = 40;
+//		a[3] = 50;
+//		
+//		for(int i=0;i<5;i++)
+//		{
+//			System.out.println(a[i]);
+//		}
+		
+		int b[] = {10,20,30,40,50,60,56};
+		
+		for(int i=0;i<b.length;i++)
+		{
+			System.out.println(b[i]);
+		}
+		
+		System.out.println("******************************");
+		
+		String sub[] = {"java","selenium","android","php","sql"};
+		
+		for(int i=0;i<sub.length;i++)
+		{
+			System.out.println(sub[i]);
+		}
+		
+		System.out.println("********************************");
+		
+		char ch[] = {'A','B','C','D'};
+
+		
+		for(int i=0;i<ch.length;i++)
+		{
+			System.out.println(ch[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
