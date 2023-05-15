6f8fb06e9023363a094518162f0d1e68c786c01d
chintan <chintan.tops@gmail.com>
2023-05-15 07:07:39 +0530

    oops

---
+++ Learning_corejava/src/oops/O003_ConstructorDemo.java
@@ -0,0 +1,54 @@
+package oops;
+
+class Emp
+{
+	Emp()
+	{
+		System.out.println("constructor called...");
+	}
+	
+	Emp(String name)
+	{
+		System.out.println("called by :"+name);
+	}
+	
+	Emp(int a)
+	{
+		System.out.println(a);
+	}
+	
+	String name;
+	String email;
+	String dept;
+	
+	Emp(String name1, String email1, String dept1)
+	{
+		name = name1;
+		email = email1;
+		dept = dept1;
+	}
+	
+	public void display()
+	{
+		System.out.println(name+" "+email+" "+dept);
+	}
+	
+	
+	
+}
+
+public class O003_ConstructorDemo {
+	public static void main(String[] args) {
+		
+//		Emp e = new Emp();
+//		Emp e1 = new Emp("Papti");
+//		Emp e2 = new Emp(10);
+//		
+		
+		Emp e1 = new Emp("Test","test@gmail.com","java");
+		e1.display();
+		
+		Emp e2 = new Emp("Sample","sample@gmail.com","Testing");
+		e2.display();
+	}
+}
