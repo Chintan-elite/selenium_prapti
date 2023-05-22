ab9d09b8ef2810742bcaa7e7b5c91f688c922146
chintan <chintan.tops@gmail.com>
2023-05-22 07:02:07 +0530

    oops

---
+++ Learning_corejava/src/oops/O010_MEthodoveriridng.java
@@ -0,0 +1,26 @@
+package oops;
+
+class P
+{
+	public void display()
+	{
+		System.out.println("Runing p class display");
+	}
+}
+
+class Q extends P
+{
+	@Override
+	public void display() {
+		System.out.println("Running q class display");
+	}
+}
+
+public class O010_MEthodoveriridng {
+	public static void main(String[] args) {
+		
+		Q q = new Q();
+		q.display();
+		
+	}
+}
