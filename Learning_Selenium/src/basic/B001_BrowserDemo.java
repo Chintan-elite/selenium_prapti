a7ca6ebf325cbd9188b9cc1178ab7850496d7f96
chintan <chintan.tops@gmail.com>
2023-06-27 08:55:13 +0530

    selenium

---
+++ Learning_Selenium/src/basic/B001_BrowserDemo.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class B001_BrowserDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+}
